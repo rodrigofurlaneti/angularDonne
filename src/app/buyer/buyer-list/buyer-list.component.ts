f4761dbe62a746809cc72b21c747174e6eede753
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2023-12-07 16:29:58 -0300

    Add test

--- src/app/buyer/buyer-list/buyer-list.component.ts
+++ src/app/buyer/buyer-list/buyer-list.component.ts
@@ -3,8 +3,6 @@ import { Router } from '@angular/router';
 import { BuyerListService } from './buyer-list.service';
 import { BuyerModel } from 'src/interface/buyer.interface';
 
-let ELEMENT_DATA: BuyerModel[];
-
 @Component({
   selector: 'buyer-list',
   templateUrl: './buyer-list.component.html',
@@ -12,30 +10,61 @@ let ELEMENT_DATA: BuyerModel[];
 })
 export class BuyerListComponent implements OnInit {
 
-  displayedColumns: string[] = ['id','name'];
+  //#region [Properties]
+  //Property DisplayedColumns
+  private _displayedColumns: string[] = ['id','name'];
+  get displayedColumns() { return this._displayedColumns; }
+  set displayedColumns(value) { this._displayedColumns = value; }
+
+  //Property DataSource
+  private _dataSource: BuyerModel[] = [];
+  get dataSource() { return this._dataSource; }
+  set dataSource(value) { this._dataSource = value; }
+
+  //Property ClickedRows
+  private _clickedRows = new Set<BuyerModel>();
+  get clickedRows() { return this._clickedRows; }
+  set clickedRows(value) { this._clickedRows = value; }
+  
+  //Property RouterString
+  private _routerString: string = '';
+  get routerString() { return this._routerString; }
+  set routerString(value) { this._routerString = value; }
+
+  //Property MessageErro
+  private _messageErro: string = '';
+  get messageErro() { return this._messageErro; }
+  set messageErro(value) { this._messageErro = value; }
+  
+  // #endregion
+
+  // #region [Constructor]
 
   constructor(private buyerListService: BuyerListService,
               private router: Router) { }
   
+  // #endregion
+
+  // #region [Methods]
+
   ngOnInit(): void {
     this.list();
   }
 
   list() {
+    this.messageErro = 'Erro ao listar os clientes!';
     this.buyerListService.list().subscribe(list => {
-      ELEMENT_DATA = list;
-      this.dataSource = ELEMENT_DATA;
+      this.dataSource = list;
     }, err => {
-      console.log('Erro ao listar os clientes', err);
+      console.log(this.messageErro, err);
     })
   }
 
-  dataSource = ELEMENT_DATA;
-  clickedRows = new Set<BuyerModel>();
-
   reply(){
-    this.router.navigate(['main']);
+    this.routerString = 'main';
+    this.router.navigate([this.routerString]);
   }
+  // #endregion
 }
 
 
